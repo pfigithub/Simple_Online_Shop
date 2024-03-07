81c32efd252c9727dcdf5f5904506e0b0644f47c
pfigithub <pampam7072@gmail.com>
2024-03-07 11:52:58 +0330

    urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for core project.
-
+"""OnlineShopping URL Configuration
+ 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from website.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+ 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home ,name='home'),
+    path('placeOrder/<str:i>/',placeOrder,name='placeOrder'),
+    path('login/', loginPage,name='login'),
+    path('logout/', logoutPage,name='logout'),
+    path('register/', registerPage,name='register'),
+    path('addProduct/', addProduct,name='addProduct'),
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
